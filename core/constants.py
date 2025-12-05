35bbaf47ebeb9aa3b16a1524b506bc3d266121c9
MazenHisham1 <108707030+MazenHisham1@users.noreply.github.com>
2025-12-05 23:13:55 +0200

    Add files via upload

---
+++ core/constants.py
@@ -0,0 +1,12 @@
+BLACK = 1
+WHITE = -1
+EMPTY = 0
+
+BOARD_SIZE = 8
+DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+# UI Colors mapped to logic
+COLOR_MAP = {
+    BLACK: "#121212",
+    WHITE: "#f5f5f5",
+    EMPTY: None
+}
