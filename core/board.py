995771d4b256d90122817abff3c74ed02a981da8
Kareem Salah <kareemherish@gmail.com>
2025-12-14 21:07:31 +0200

    board.py

--- core/board.py
+++ core/board.py
@@ -1,18 +1,20 @@
 import copy
-from core.constants import BLACK, WHITE, EMPTY,BOARD_SIZE,DIRECTIONS
+from .constants import BLACK, WHITE, EMPTY,BOARD_SIZE,DIRECTIONS
 
 class Board:
-    def __init__(self):
+    def _init_(self):
         self.size = BOARD_SIZE
-        self.grid = [[EMPTY for _ in range(8)] for _ in range(8)]
+        self.grid = [[EMPTY for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
         self._setup_board()
 
-    def _setup_board(self): #ءINITIAL SETUP
+    #INITIAL STATE
+    def _setup_board(self):
         self.grid[3][3] = WHITE
         self.grid[3][4] = BLACK
         self.grid[4][3] = BLACK
         self.grid[4][4] = WHITE
 
+    #Ensure the move on the board
     def is_on_board(self, r, c):
         return 0 <= r < self.size and 0 <= c < self.size
 
@@ -22,14 +24,16 @@ class Board:
             for c in range(self.size):
                 if self.is_valid_move(r, c, player):
                     moves.append((r, c))
-        return moves
+        
+        return moves #List of valid moves for the player
 
     def is_valid_move(self, r, c, player):
+        # base condition
         if self.grid[r][c] != EMPTY:
             return False
 
-        opponent = -player
         directions = DIRECTIONS
+        opponent = -player
 
         for dr, dc in directions:
             rr, cc = r + dr, c + dc
@@ -42,33 +46,57 @@ class Board:
 
             if found_opponent and self.is_on_board(rr, cc) and self.grid[rr][cc] == player:
                 return True
+        
         return False
 
+   
     def make_move(self, r, c, player):
-        """Returns a new Board instance with the move applied."""
+        """Returns a new Board instance. Used by GameState to keep history."""
         if not self.is_valid_move(r, c, player):
             return None
-
         new_board = copy.deepcopy(self)
-        new_board.grid[r][c] = player
+        new_board.apply_move(r, c, player) 
+        return new_board
+
+    # --- FAST (For AI) ---
+    def apply_move(self, r, c, player):
+        """
+        Modifies the board IN-PLACE. 
+        Returns a list of flipped pieces so we can Undo later.
+        """
+        self.grid[r][c] = player
         opponent = -player
         directions = DIRECTIONS
+        flipped_pieces = []
+
         for dr, dc in directions:
             rr, cc = r + dr, c + dc
-            flips = []
+            potential_flips = []
 
-            while new_board.is_on_board(rr, cc) and new_board.grid[rr][cc] == opponent:
-                flips.append((rr, cc))
+            while self.is_on_board(rr, cc) and self.grid[rr][cc] == opponent:
+                potential_flips.append((rr, cc))
                 rr += dr
                 cc += dc
 
-            if new_board.is_on_board(rr, cc) and new_board.grid[rr][cc] == player:
-                for fr, fc in flips:
-                    new_board.grid[fr][fc] = player
+            if potential_flips and self.is_on_board(rr, cc) and self.grid[rr][cc] == player:
+                for fr, fc in potential_flips:
+                    self.grid[fr][fc] = player
+                    flipped_pieces.append((fr, fc))
         
-        return new_board
+        return flipped_pieces
+
+    def undo_move(self, r, c, flipped_pieces, player):
+        """Reverts a move using the list of flipped pieces."""
+        self.grid[r][c] = EMPTY
+        opponent = -player
+        for r_flip, c_flip in flipped_pieces:
+            self.grid[r_flip][c_flip] = opponent
 
     def get_score(self):
         b = sum(row.count(BLACK) for row in self.grid)
         w = sum(row.count(WHITE) for row in self.grid)
         return b, w
+    
+    def get_hash(self):
+        """Returns a hashable representation of the board for caching."""
+        return tuple(tuple(row) for row in self.grid)
