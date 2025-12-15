0d955242606eed37c14f39185eaf01a263661e06
MrJazzar <mrjazzar2005@gmail.com>
2025-12-15 19:30:39 +0200

    Update AdvancedAgent.py

--- ai/AdvancedAgent.py
+++ ai/AdvancedAgent.py
@@ -1,12 +1,11 @@
-import random
-import math
-
-
-from ai.algorithm import minimax, move_generator
+from ai.base_agent import BaseAgent
+from ai.algorithm import Minimax
 
 class AdvancedAgent(BaseAgent):
-    """Uses Minimax with higher depth (e.g., 4)."""
-    def get_move(self, game_state):
-        # Depth 4 أصعب ويأخذ وقتاً أطول قليلاً في التفكير
-        _, move = minimax(game_state, 4, -math.inf, math.inf, True, self.color)
-        return move
+    def __init__(self, color):
+        super().__init__(color)
+        self.ai = Minimax()
+
+    def get_move(self, board):
+        
+        return self.ai.get_move_using_minimax(board, self.color, depth=5)
