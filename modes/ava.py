d0d1c7e37f1d6ecfd7073045a3800d5b703ae716
amrk04282-lgtm <amrk04282@gmail.com>
2025-12-15 23:08:50 +0200

    Update and rename agent_vs_agent.py to ava.py

--- modes/ava.py
+++ modes/ava.py
@@ -2,7 +2,7 @@ from ai.beginner_agent import BeginnerAgent
 from ai.advanced_agent import AdvancedAgent
 from core.constants import BLACK, WHITE
 
-class ava:
+class AvAMode:
     @staticmethod
     def get_agents():  
         return BeginnerAgent(BLACK), AdvancedAgent(WHITE)
