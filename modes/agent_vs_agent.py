b5d385db7527e770ba0a725ccf3861b8fe974668
amrk04282-lgtm <amrk04282@gmail.com>
2025-12-15 20:39:56 +0200

    Update agent_vs_agent.py

--- modes/agent_vs_agent.py
+++ modes/agent_vs_agent.py
@@ -2,7 +2,7 @@ from ai.beginner_agent import BeginnerAgent
 from ai.advanced_agent import AdvancedAgent
 from core.constants import BLACK, WHITE
 
-class avaMode:
+class ava:
     def get_agents():  
         return BeginnerAgent(BLACK), AdvancedAgent(WHITE)
 
